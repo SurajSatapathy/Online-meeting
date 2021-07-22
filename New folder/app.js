1f3a790c6e4b2aabb7cc2f7e046b7df858c3e249
SurajSatapathy <87822999+SurajSatapathy@users.noreply.github.com>
2021-07-22 21:45:53 +0530

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+app.get("/", function(req, res) {
+
+    var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+var day = today.getDay();
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case  6:
+      day = "Saturday";
+  }
+  var time =   + today.getHours() + ":"
+  + today.getMinutes() ;
+  
+
+
+
+today = day + ' - '  + time   +  ' - ' + mm + '/' + dd + '/' + yyyy;
+
+res.render("list", {date: today})
+
+
+});
+
+
+app.listen(3008, function(){
+    console.log("server has started on port 3008");
+});
+
+
